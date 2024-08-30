19864fa8286e1c7354d06fc9954f3d8b47f71f67
Soumikdey81 <122925334+Soumikdey81@users.noreply.github.com>
2024-08-30 08:46:00 +0530

    Add files via upload

---
+++ task1/public/js/index.js
@@ -0,0 +1,5 @@
+const navbarCollapse = document.getElementById('navbar-collapse');
+function navbar_collapse(){
+    navbarCollapse.classList.toggle('-translate-x-full');
+    navbarCollapse.classList.toggle('translate-x-0');
+}
